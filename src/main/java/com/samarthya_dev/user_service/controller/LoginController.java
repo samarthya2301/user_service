950b2b526374897f85036278e402d37aa4c1b2a2
samarthya2301 <samarthya2301@gmail.com>
2025-12-15 17:19:01 +0530

    refresh-token endpoint implemented

--- src/main/java/com/samarthya_dev/user_service/controller/LoginController.java
+++ src/main/java/com/samarthya_dev/user_service/controller/LoginController.java
@@ -2,18 +2,19 @@ package com.samarthya_dev.user_service.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.samarthya_dev.user_service.controller.flow_group.login.FlowLogin;
+import com.samarthya_dev.user_service.controller.flow_group.login.FlowRefresh;
 import com.samarthya_dev.user_service.dto.request.LoginRequest;
-import com.samarthya_dev.user_service.dto.request.RefreshRequest;
 import com.samarthya_dev.user_service.dto.response.LoginResponse;
 import com.samarthya_dev.user_service.service.login.LoginService;
 
-import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -30,7 +31,7 @@ public class LoginController {
 	}
 
 	@PostMapping(path = "/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
+	public ResponseEntity<LoginResponse> login(@RequestBody @Validated(FlowLogin.class) LoginRequest loginRequest) {
 
 		log.info("Request Invoked on Endpoint: /login");
 		log.debug(loginRequest.toString());
@@ -47,8 +48,20 @@ public class LoginController {
 	}
 
 	@PostMapping(path = "/refresh-token", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<LoginResponse> login(@RequestBody @Valid RefreshRequest refreshRequest) {
-		return null;
+	public ResponseEntity<LoginResponse> refresh(@RequestBody @Validated(FlowRefresh.class) LoginRequest refreshRequest) {
+
+		log.info("Request Invoked on Endpoint: /refresh-token");
+		log.debug(refreshRequest.toString());
+
+		LoginResponse refreshResponse = loginService.refresh(refreshRequest);
+		log.info("Request Processed for Endpont: /refresh-token");
+		log.debug(refreshResponse.toString());
+
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.contentType(MediaType.APPLICATION_JSON)
+			.body(refreshResponse);
+
 	}
 	
 }
