fcdc9332363454b9c916e361239c0434458e477b
samarthya2301 <samarthya2301@gmail.com>
2025-12-17 18:44:25 +0530

    auth test updates

---
+++ src/main/java/com/samarthya_dev/user_service/controller/CheckAndTestController.java
@@ -0,0 +1,29 @@
+package com.samarthya_dev.user_service.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.samarthya_dev.user_service.dto.request.TestAuthRequest;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@Slf4j
+@RestController
+public class CheckAndTestController {
+
+	@GetMapping(path = "/healthCheck", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String healthCheck() {
+		return "user-service API is up and running!";
+	}
+
+	@PostMapping(path = "/test-auth", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
+	public String postMethodName(@RequestBody @Valid TestAuthRequest testAuthRequest) {
+		return "User Authentication Successful: " + testAuthRequest.getEmail();
+	}
+	
+}
---
+++ src/main/java/com/samarthya_dev/user_service/dto/request/TestAuthRequest.java
@@ -0,0 +1,32 @@
+package com.samarthya_dev.user_service.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@Builder
+@AllArgsConstructor
+@ToString(exclude = {"accessToken"})
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TestAuthRequest {
+
+	@Email(message = "Invalid E-Mail Format")
+	@NotNull(message = "E-Mail cannot be NULL")
+	@NotEmpty(message = "E-Mail cannot be Empty")
+	@JsonProperty("email")
+	private String email;
+
+	@NotNull(message = "Access Token cannot be NULL")
+	@NotEmpty(message = "Access Token cannot be Empty")
+	@JsonProperty("accessToken")
+	private String accessToken;
+	
+}
