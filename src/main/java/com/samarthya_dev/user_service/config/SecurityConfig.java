a2bc11269f7452c0962ee2a19118c32190c7aa7b
samarthya2301 <samarthya2301@gmail.com>
2026-01-03 16:34:21 +0530

    healthCheck removed from auth

--- src/main/java/com/samarthya_dev/user_service/config/SecurityConfig.java
+++ src/main/java/com/samarthya_dev/user_service/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
 			.authorizeHttpRequests(
 				auth -> auth
 					.requestMatchers(
+						"/healthCheck",
 						"/register",
 						"/otp/request",
 						"/otp/verify",
