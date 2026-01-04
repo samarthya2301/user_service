5dfaab9c9bf0d940961fbd88424d0024d271912e
samarthya2301 <samarthya2301@gmail.com>
2026-01-04 18:48:48 +0530

    login method refactored with optional

--- src/main/java/com/samarthya_dev/user_service/service/login/LoginServiceImpl.java
+++ src/main/java/com/samarthya_dev/user_service/service/login/LoginServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 
 import com.samarthya_dev.user_service.dto.request.LoginRequest;
 import com.samarthya_dev.user_service.dto.response.LoginResponse;
-import com.samarthya_dev.user_service.entity.user.UserEntity;
 import com.samarthya_dev.user_service.entity.user.UserStatus;
 import com.samarthya_dev.user_service.repository.RefreshTokenRepository;
 import com.samarthya_dev.user_service.repository.UserRepository;
@@ -26,53 +25,67 @@ public class LoginServiceImpl implements LoginService {
 	private final RefreshService refreshService;
 	private final RefreshTokenRepository refreshTokenRepository;
 
+	private LoginResponse logMessageAndCreateLoginResponse(String logMessage, String responseMessage) {
+
+		log.info(logMessage);
+
+		return LoginResponse
+				.builder()
+				.message(responseMessage)
+				.build();
+
+	}
+
 	@Override
 	public LoginResponse login(LoginRequest loginRequest) {
 
 		log.info("Login Service Invoked");
 
 		log.info("Searching user by E-Mail in Database");
-		UserEntity userEntity = userRepository.findByEmail(loginRequest.getEmail()).orElse(null);
 
-		if (userEntity == null || !passwordEncoder.matches(loginRequest.getPassword(), userEntity.getHashedPassword())) {
+		return userRepository
+			.findByEmail(loginRequest.getEmail())
+			.map(userEntity -> {
 
-			log.info("Queried User not found in Database or the Password was Incorrect");
+				if ( !passwordEncoder.matches(loginRequest.getPassword(), userEntity.getHashedPassword())) {
+					return logMessageAndCreateLoginResponse(
+						"Queried User not found in Database or the Password was Incorrect",
+						"E-Mail or Password is Incorrect. Please Try Again"
+					);
+				}
 
-			return LoginResponse
-				.builder()
-				.message("E-Mail or Password is Incorrect. Please Try Again")
-				.build();
+				if ( !userEntity.getEmailVerified() || userEntity.getStatus() != UserStatus.ACTIVE) {
+					return logMessageAndCreateLoginResponse(
+						"Queried User's E-Mail is not Verified and is DISABLED",
+						"E-Mail is not Verified. Please Verify E-Mail before proceeding"
+					);
+				}
 
-		}
+				log.info("Valid User with Correct Password and Verified E-Mail found in Database");
 
-		if ( !userEntity.getEmailVerified() || userEntity.getStatus() != UserStatus.ACTIVE) {
+				log.info("Generating Refresh Token for User");
+				String refreshToken = refreshService.generateToken(userEntity);
+				log.info("Refresh Token Created for User");
 
-			log.info("Queried User's E-Mail is not Verified and is DISABLED");
+				log.info("Generating JWT for User");
+				String jwtToken = jwtService.generateToken(userEntity);
+				log.info("JWT Created for User");
 
-			return LoginResponse
-				.builder()
-				.message("E-Mail is not Verified. Please Verify E-Mail before proceeding")
-				.build();
+				return LoginResponse
+					.builder()
+					.accessToken(jwtToken)
+					.refreshToken(refreshToken)
+					.tokenType("Bearer")
+					.message("User Logged In Successfully")
+					.build();
 
-		}
-
-		log.info("Valid User with Correct Password and Verified E-Mail found in Database");
-
-		log.info("Generating Refresh Token for User");
-		String refreshToken = refreshService.generateToken(userEntity);
-		log.info("Refresh Token Created for User");
-
-		log.info("Generating JWT for User");
-		String jwtToken = jwtService.generateToken(userEntity);
-		log.info("JWT Created for User");
-
-		return LoginResponse
-			.builder()
-			.accessToken(jwtToken)
-			.refreshToken(refreshToken)
-			.tokenType("Bearer")
-			.message("User Logged In Successfully")
-			.build();
+			})
+			.orElseGet(() -> {
+				return logMessageAndCreateLoginResponse(
+					"Queried User not found in Database or the Password was Incorrect",
+					"E-Mail or Password is Incorrect. Please Try Again"
+				);
+			});
 
 	}
 
