f88d6f01cda9d96ae9017753db72b33b8f3d60f7
samarthya2301 <samarthya2301@gmail.com>
2026-01-15 19:34:27 +0530

    removed enabled scheduling from app

--- src/main/java/com/samarthya_dev/user_service/UserServiceApplication.java
+++ src/main/java/com/samarthya_dev/user_service/UserServiceApplication.java
@@ -2,9 +2,7 @@ package com.samarthya_dev.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class UserServiceApplication {
 	public static void main(String[] args) {
