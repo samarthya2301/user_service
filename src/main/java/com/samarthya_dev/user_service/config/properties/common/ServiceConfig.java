64521abad73bdce97265f24553f9c26db5923c2a
samarthya2301 <samarthya2301@gmail.com>
2026-01-20 18:44:22 +0530

    service properties added

---
+++ src/main/java/com/samarthya_dev/user_service/config/properties/common/ServiceConfig.java
@@ -0,0 +1,62 @@
+package com.samarthya_dev.user_service.config.properties.common;
+
+import lombok.Value;
+
+import java.time.Duration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Value
+@ConfigurationProperties(prefix = "service")
+public class ServiceConfig {
+
+    Otp otp;
+    Token token;
+
+    @Value
+    public static class Otp {
+
+        Duration expireAfter;
+
+    }
+
+    @Value
+    public static class Token {
+
+        Jwt jwt;
+        Refresh refresh;
+
+        @Value
+        public static class Jwt {
+
+            Claim claim;
+            Duration expireAfter;
+
+            @Value
+            public static class Claim {
+
+                Key key;
+
+                @Value
+                public static class Key {
+
+                    String userId;
+                    String userEmail;
+                    String userRole;
+
+                }
+
+            }
+
+        }
+
+        @Value
+        public static class Refresh {
+
+            Duration expireAfter;
+
+        }
+
+    }
+
+}
