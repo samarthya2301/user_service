4e7fb17f168892cc9770cd3df75ee8bb5feae68d
samarthya2301 <samarthya2301@gmail.com>
2026-01-22 18:51:28 +0530

    basic auth disabled

--- src/main/java/com/samarthya_dev/user_service/UserServiceApplication.java
+++ src/main/java/com/samarthya_dev/user_service/UserServiceApplication.java
@@ -2,8 +2,9 @@ package com.samarthya_dev.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
